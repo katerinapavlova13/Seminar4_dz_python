88018224170901363700ba7dddbb67449021aa32
EkaterinaPavlova <malinakata@gmail.com>
2022-10-06 17:51:36 +0400

    Save Task1

--- Task1.py
+++ Task1.py
@@ -4,3 +4,24 @@
 # при d = 0.1, π = 3.1
 # при d = 0.00001, π = 3.14154
 # d от 0.1 до 0.0000000001
+
+import math
+
+d = float(input("Введите число от 0.1 до 0.0000000001: "))
+def get_count(d):
+    str_d = str(d)
+    if "." in str_d:
+        return len(str_d.split(".")[1].rstrip("0"))
+    else:
+        return 0
+
+res = get_count(d)
+print(res)
+
+print(round(math.pi, res))
+
+
+
+
+
+
